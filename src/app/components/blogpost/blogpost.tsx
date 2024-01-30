fc41f66cd20ee67aee22c2ade90a9e59115f1780
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-01-31 00:05:49 +0200

    styling

--- src/app/components/blogpost/blogpost.tsx
+++ src/app/components/blogpost/blogpost.tsx
@@ -4,7 +4,11 @@ import { Label } from "@/app/components/ui/blogpost-label/label";
 import { Input } from "@/app/components/ui/blogpost-input/input";
 import { Textarea } from "@/app/components/ui/blogpost-textarea/textarea";
 import { Button } from "@/app/components/ui/button/button";
+import PlusIcon from "../../../../public/icons/plusicon";
 import "./blogpost.scss";
+
+const handleFileChange = (event: any) => {
+}
 
 export function BlogPostComponent() {
   return (
@@ -21,10 +25,18 @@ export function BlogPostComponent() {
           <Label htmlFor="content">Content</Label>
           <Textarea placeholder="Enter the post content" />
         </div>
-        <div className="image-upload-container">
-          <p>Upload an image for your post</p>
-        
+        <div className="input-group image-upload-container">
+          <div className="image-upload">
+            <PlusIcon className="plus-icon" />
+            <p>Drag & drop or click to upload images</p>
+            <Input id="image" type="file" className="sr-only"
+            onChange={handleFileChange}/>
+            <Label htmlFor="image" className="upload-label">
+              Choose a file
+            </Label>
+          </div>
         </div>
+
         <Button
           className="submit-button"
           type="submit"
