bb19f747c8f1c5968a675a665b73573ea2ddfb82
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-01-31 22:57:52 +0200

    toggle theme slider

--- src/app/components/blogpost/blogpost.tsx
+++ src/app/components/blogpost/blogpost.tsx
@@ -6,6 +6,7 @@ import {Textarea} from '@/app/components/ui/blogpost-textarea/textarea';
 import {Button} from '@/app/components/ui/common/button/button';
 import PlusIcon from '@/app/components/ui/icons/plusicon';
 import './blogpost.scss';
+import Switch from '@/app/components/ui/theme-toggle/toggle';
 
 const handleFileChange = (event: any) => {};
 
@@ -13,6 +14,7 @@ export function BlogPostComponent() {
   return (
     <div className="component-container">
       <div className="header">
+        <Switch id="theme-toggle" />
         <h2>Create a blog post</h2>
       </div>
       <div className="form-container">
