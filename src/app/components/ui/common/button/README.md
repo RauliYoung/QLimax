750069ca72272646eb92450a924030719a0b2793
Joonas Jouttijärvi <92953234+joonasjouttijarvi@users.noreply.github.com>
2024-02-01 19:39:56 +0200

    Development (#3)
    * blog posting frontend
    
    Blogpost creation frontend
    
    * package
    
    * styling
    
    * refactor
    
    * eslit and prettier configs
    
    * icons for theme toggle
    
    * responsive styling for blogpost
    
    * toggle theme slider
    
    * fix toggle jump after click
    
    * icons for theme toggle
    
    * readme for button
    
    ---------
    
    Co-authored-by: Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>

---
+++ src/app/components/ui/common/button/README.md
@@ -0,0 +1,16 @@
+### Button Component
+##### Overview
+
+The Button component is a versatile and customizable button component. 
+
+- Extensible Props: Inherits properties from React.ButtonHTMLAttributes<HTMLButtonElement>.
+##### Variants: 
+- Supports variants  
+    - ' default', 'destructive',"secondary," 'outline', 'ghost', 'link', "submit",
+##### Sizes:
+- Available in different sizes 
+    - ('default', 'sm', 'lg', 'icon').
+- Custom Element Rendering: Utilizes asChild prop to render custom elements while maintaining button behavior and styling.
+- Ref Forwarding: Supports ref forwarding to access the DOM node.
+
+ 
---
+++ src/app/components/ui/theme-toggle/toggle.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './toggle.scss';
+import SunIcon from '@/app/components/ui/icons/sunicon';
+import MoonIcon from '@/app/components/ui/icons/moonicon';
+
+type SwitchProps = {
+  id: string;
+  defaultChecked?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+};
+
+const Switch: React.FC<SwitchProps> = ({ id, defaultChecked = false, onChange, className }) => {
+  const [isChecked, setIsChecked] = useState(defaultChecked);
+
+  useEffect(() => {
+    setIsChecked(defaultChecked);
+  }, [defaultChecked]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+    if(onChange) {
+      onChange(event);
+    }
+  };
+
+  return (
+    <label htmlFor={id} className={`switch ${className}`}>
+      {!isChecked && <SunIcon className="icon sun-icon" />}
+      <input
+        type="checkbox"
+        id={id}
+        checked={isChecked}
+        onChange={handleChange}
+        aria-label="Toggle between themes"
+      />
+      <span className="slider round"></span>
+      {isChecked && <MoonIcon className="icon moon-icon" />}
+    </label>
+  );
+};
+
+export default Switch;
+
