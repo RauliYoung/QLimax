364fe2eb32208579fe7da7edb292dcbd63c48c5e
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-01 21:38:03 +0200

    chakra dark/light toggle
    chakra dark/light toggle
    
    merge feature/toggle

--- src/app/components/ui/theme-toggle/toggle.tsx
+++ src/app/components/ui/theme-toggle/toggle.tsx
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './toggle.scss';
 import SunIcon from '@/app/components/ui/icons/sunicon';
 import MoonIcon from '@/app/components/ui/icons/moonicon';
+import { useColorMode } from '@chakra-ui/react';
 
-type SwitchProps = {
+type ToggleProps = {
   id: string;
   defaultChecked?: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
 };
 
-const Switch: React.FC<SwitchProps> = ({ id, defaultChecked = false, onChange, className }) => {
+const Toggle: React.FC<ToggleProps> = ({ id, defaultChecked = false, onChange, className }) => {
+  const { colorMode , toggleColorMode } = useColorMode();
   const [isChecked, setIsChecked] = useState(defaultChecked);
 
   useEffect(() => {
-    setIsChecked(defaultChecked);
-  }, [defaultChecked]);
+    setIsChecked(colorMode === 'dark');
+  }, [colorMode]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(event.target.checked);
+    toggleColorMode();
     if(onChange) {
       onChange(event);
     }
@@ -40,5 +43,5 @@ const Switch: React.FC<SwitchProps> = ({ id, defaultChecked = false, onChange, c
   );
 };
 
-export default Switch;
+export default Toggle;
 
