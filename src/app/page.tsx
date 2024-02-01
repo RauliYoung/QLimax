df6210685fd9775bf818391ae3570856037b8e6f
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-01 14:05:05 +0200

    icons for theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import {BlogPostComponent} from '@/app/components/blogpost/blogpost';
-import MoonIcon from '@/app/components/ui/icons/moonicon';
-import SunIcon from '@/app/components/ui/icons/sunicon';
 export default function Main() {
   return (
     <div>
-      <MoonIcon />
-      <SunIcon />
       <BlogPostComponent />
     </div>
   );
