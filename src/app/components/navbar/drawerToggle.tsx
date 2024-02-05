819f20b4e2a0a7f0ee16d5f728025dead8f467d4
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-05 12:12:25 +0200

    navbar styling
    navbar button styling
    
    navbar button
    
    navigation skele and buttons
    
    navigation skele and buttons

--- src/app/components/navbar/drawerToggle.tsx
+++ src/app/components/navbar/drawerToggle.tsx
@@ -1,3 +1,4 @@
+// TODO: reformat as import * as Chakra from '@chakra-ui/react';
 import {
   Drawer,
   DrawerBody,
@@ -6,16 +7,50 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Button,
+  MenuButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
 } from '@chakra-ui/react';
+import { BsFillBookmarkFill, BsFillPersonFill } from 'react-icons/bs';
 import React from 'react';
 import { IconButton } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
-import { PlusSquareIcon } from '@chakra-ui/icons';
+import {
+  EditIcon,
+  PlusSquareIcon,
+  SettingsIcon,
+  ArrowForwardIcon,
+} from '@chakra-ui/icons';
+import './drawerToggle.scss';
 
 export function DrawerToggle() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isDrawerOpen,
+    onOpen: onDrawerOpen,
+    onClose: onDrawerClose,
+  } = useDisclosure();
+  const {
+    isOpen: isModalOpen,
+    onOpen: onModalOpen,
+    onClose: onModalClose,
+  } = useDisclosure();
 
   const accountname = 'foobar';
+
+  // TODO: implement logout 
+
+  const handleLogout = () => {
+    console.log('logout');
+  };
 
   return (
     <>
@@ -24,20 +59,70 @@ export function DrawerToggle() {
         variant="solid"
         className="drawer-toggle"
         icon={<PlusSquareIcon />}
-        onClick={onOpen}
+        onClick={onDrawerOpen}
       />
 
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Hello ! {accountname}</DrawerHeader>
+      <Drawer isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}>
+        <DrawerOverlay className="drawer-overlay" />
+        <DrawerContent className="drawer-content">
+          <DrawerCloseButton className="drawer-close-button" />
+          <DrawerHeader className="drawer-header">
+            Hello ! {accountname}
+          </DrawerHeader>
+          <DrawerBody className="drawer-body">
+            <Menu>
+              <MenuButton as={Button} leftIcon={<EditIcon />}>
+                Blogs
+              </MenuButton>
+              <MenuList>
+                <MenuItem>fizz</MenuItem>
+                <MenuItem>buzz</MenuItem>
+                <MenuItem>fizzbuzz</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton as={Button} leftIcon={<BsFillPersonFill />}>
+                Profile
+              </MenuButton>
+              <MenuList>
+                <MenuItem icon={<SettingsIcon />}>Settings</MenuItem>
+                <MenuItem icon={<ArrowForwardIcon />} onClick={onModalOpen}>
+                  Logout
+                </MenuItem>
+              </MenuList>
+            </Menu>
 
-          <DrawerBody></DrawerBody>
+            <Button
+              leftIcon={<BsFillBookmarkFill />}
+              className="drawer-button"
+              variant="outline"
+            >
+              Bookmarks
+            </Button>
+          </DrawerBody>
 
-          <DrawerFooter></DrawerFooter>
+          <DrawerFooter className="drawer-footer"></DrawerFooter>
         </DrawerContent>
       </Drawer>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isModalOpen}
+        onClose={onModalClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Are you sure you want to log out</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}></ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={handleLogout}>
+              Logout
+            </Button>
+            <Button onClick={onModalClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
