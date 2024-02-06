2912a9149109dd0d563fb64536d1c2411f5d2eae
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2024-02-06 14:44:41 +0200

    Sign up page jeejee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ import {
   CardHeader,
 } from '@chakra-ui/react';
 import testData from '../../testData/testPosts.json';
+<<<<<<< HEAD
 import {BlogPostComponent} from './components/blogpost/blogpost';
+=======
+import SignUp from './signup';
+>>>>>>> 9f9a7da (Sign up page)
 
 interface Post {
   title: string;
@@ -17,10 +21,65 @@ interface Post {
 }
 export default function Home() {
   return (
+<<<<<<< HEAD
     <>
 
     <BlogPostComponent />
     </>
    
+=======
+    <SignUp />
+
+    // <Flex as='main' direction='column'>
+    //   <Flex
+    //     as='section'
+    //     direction='column'
+    //     p='1rem'
+    //     justifyContent='center'
+    //     alignItems='center'
+    //   >
+    //     <Heading as='h1'>Hello world, is an old message</Heading>
+    //     <Text as='p'>
+    //       This is a paragraph, and it is the child of a flex container with
+    //       direction column
+    //     </Text>
+    //     <Text as='p'>
+    //       Whereas disregard and contempt for human rights have resulted
+    //     </Text>
+    //   </Flex>
+    //   <Flex maxW='100%' justifyContent='center'>
+    //     <Flex
+    //       as='section'
+    //       wrap='wrap'
+    //       justifyContent='center'
+    //       maxW='798px'
+    //       gap='1rem'
+    //       m='auto'
+    //     >
+    //       {testData
+    //         .map((post: Post) => ({
+    //           title: post.title,
+    //           header: post.header,
+    //           body: post.body,
+    //         }))
+    //         .map((post: Post) => (
+    //           <Card w='250px' h='300px'>
+    //             <CardBody overflow='auto'>
+    //               <Heading as='h2' fontSize={'28px'}>
+    //                 {post.title}
+    //               </Heading>
+    //               <Text as='p' fontSize='15px'>
+    //                 {post.header}
+    //               </Text>
+    //               <Text as='p' fontSize='15px'>
+    //                 {post.body}
+    //               </Text>
+    //             </CardBody>
+    //           </Card>
+    //         ))}
+    //     </Flex>
+    //   </Flex>
+    // </Flex>
+>>>>>>> 9f9a7da (Sign up page)
   );
 }
