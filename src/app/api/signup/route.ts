1e55e4c94c350b691d0bd2095ce73710576a3fa1
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-12 22:35:58 +0200

    crypt the pw

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,13 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 import mongoConnect from '@/app/lib/db';
 import User from '@/app/models/userModel';
+import bcrypt from 'bcrypt';
 
 export async function POST(req: NextRequest) {
   await mongoConnect();
 
   try {
     const data = await req.json();
-    const user = new User(data);
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+    const user = new User({...data, password: hashedPassword});
     await user.save();
 
     return new NextResponse(
