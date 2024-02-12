2c0502979cf9ca991ced671bbe8c248d41835186
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-13 01:06:33 +0200

    login etc
    login, logout and user context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-
-import { customTheme as theme } from '../../themes/theme';
-import { ChakraProvider } from '@chakra-ui/react';
+import {customTheme as theme} from '../../themes/theme';
+import {ChakraProvider} from '@chakra-ui/react';
 import React from 'react';
-import { UserProvider } from './providers/userProvider';
+import {UserProvider} from './providers/userProvider';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({children}: {children: React.ReactNode}) {
   console.log(theme);
   return (
     <ChakraProvider theme={theme}>
-      <UserProvider>
-        {children}
-      </UserProvider>
+      <UserProvider>{children}</UserProvider>
     </ChakraProvider>
   );
 }
