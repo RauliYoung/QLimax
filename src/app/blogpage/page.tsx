6676f7e1c98ad0ac23f3827b9fcba9ad0d78d7a7
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-16 21:28:10 +0200

    blogpost read page

--- src/app/blogpage/page.tsx
+++ src/app/blogpage/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React from 'react';
 import {
   Box,
@@ -9,44 +9,63 @@ import {
   Divider,
   Button,
   Textarea,
+  IconButton,
+  Flex,
 } from '@chakra-ui/react';
 import {useState} from 'react';
+import {BsHandThumbsUp, BsHeartFill} from 'react-icons/bs';
 
 const BlogPostPage = () => {
-
-  const date = new Date().toLocaleDateString( 'fi-FI');
-  const [isSubmitting]= useState(false);
+  const date = new Date().toLocaleDateString('fi-FI');
+  const [isSubmitting] = useState(false);
 
   return (
     <Container py={8}>
       <VStack spacing={8} alignItems="flex-start">
         <Heading>BLOGPOST</Heading>
-        <Text>
-          <h2>Blog Post Title</h2>
+          <h1>Blog Post Title</h1>
           <p>Posted on: {date}</p>
           <p>Author: Fahey Schmidt</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+          <Divider />
+        <Text>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </Text>
         <Divider />
+        <Flex alignItems="center" justifyContent="space-between" w="full">
         <Heading size="md">Comments</Heading>
+          <IconButton aria-label="like" icon={<BsHeartFill />} />
+        </Flex>
         <Box w="full">
           <Textarea placeholder="Write a comment..." mb={4} />
-          <Button colorScheme="blue"  isLoading={isSubmitting} loadingText='Submitting' spinnerPlacement='start'>Submit</Button>
+          <Button
+            colorScheme="blue"
+            isLoading={isSubmitting}
+            loadingText="Submitting"
+            spinnerPlacement="start"
+          >
+            Submit
+          </Button>
         </Box>
         <VStack spacing={4} alignItems="flex-start" w="full">
           <Text fontWeight="bold">Noemy.Blick</Text>
-          <Text>comments Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Text>
+          <Text>
+            comments Lorem ipsum dolor sit amet, qui minim labore adipisicing
+            minim sint cillum sint consectetur cupidatat.
+          </Text>
           <Divider />
           <Text fontWeight="bold">Vito_Altenwerth28</Text>
-          <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Text>
+          <Text>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </Text>
         </VStack>
       </VStack>
     </Container>
