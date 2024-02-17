4fde4cbba581bb79bbca8dad31ae5dbf66264915
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-02-17 14:32:10 +0200

    small refuck

---
+++ src/app/contexts/usercontext.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface UserContextProps {
+  user: null | {token: string};
+  setUser: React.Dispatch<React.SetStateAction<{token: string} | null>>;
+}
+
+export const UserContext = React.createContext<UserContextProps>({
+  user: null,
+  setUser: () => {}, 
+});
+
+export type UserContextType = UserContextProps;
