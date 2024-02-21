4d2c1229dd837093fa18448d8eaeddef6e2017fa
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2024-02-21 22:07:16 +0200

    ok

--- src/app/api/graphql/datasources/index.ts
+++ src/app/api/graphql/datasources/index.ts
@@ -1,10 +1,9 @@
-import { MongoDataSource } from 'apollo-datasource-mongodb';
+import {MongoDataSource} from 'apollo-datasource-mongodb';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { ObjectId } from 'mongodb';
+import {ObjectId} from 'mongodb';
 import UserModel from '@/app/models/userModel';
 import PostModel from '@/app/models/postModel';
-import {ALL} from 'dns';
 
 interface UserDocument {
   _id: ObjectId;
@@ -15,7 +14,7 @@ interface PostDocument {
   _id: ObjectId;
   title: string;
   content: string;
-  tags: { tag: string; color: string }[];
+  tags: {tag: string; color: string}[];
   createdAt: Date;
   updatedAt: Date;
   isPublished: boolean;
@@ -32,7 +31,7 @@ export class Users extends MongoDataSource<UserDocument> {
     }
   }
 
-  async createUser({ input }: any): Promise<UserDocument> {
+  async createUser({input}: any): Promise<UserDocument> {
     try {
       const hashedPassword = await bcrypt.hash(input.password, 12);
       const newUser = await UserModel.create({
@@ -45,17 +44,11 @@ export class Users extends MongoDataSource<UserDocument> {
     }
   }
 
-<<<<<<< HEAD
   async updateUser({input}: any) {
-    console.log(input);
-=======
-  async updateUser({ input }: any) {
->>>>>>> development
     try {
-      // const hashedPassword = await bcrypt.hash(input.password, 12);
       const updatedUser = await UserModel.findByIdAndUpdate(
         input.id,
-        { ...input },
+        {...input},
         {
           new: true,
         }
@@ -66,28 +59,24 @@ export class Users extends MongoDataSource<UserDocument> {
     }
   }
 
-<<<<<<< HEAD
   async signIn({email, password}: {email: string; password: string}) {
-=======
-  async signIn({ email, password }: { email: string; password: string }) {
->>>>>>> development
     try {
-      const user = await UserModel.findOne({ email });
+      const user = await UserModel.findOne({email});
       if (!user) {
         throw new Error('User not found');
       }
 
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET ?? '', {
+      const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET ?? '', {
         expiresIn: '4h',
       });
 
-      return { token, id: user._id};
+      return {token, id: user._id};
     } catch (error) {
       throw new Error('Failed to sign in');
     }
   }
 
-  async deleteUser({ id }: { id: string }): Promise<string> {
+  async deleteUser({id}: {id: string}): Promise<string> {
     try {
       await UserModel.findByIdAndDelete(id);
       return 'User deleted successfully';
@@ -113,7 +102,7 @@ export class Posts extends MongoDataSource<PostDocument> {
     }
   }
 
-  async createPost({ input }: any): Promise<PostDocument> {
+  async createPost({input}: any): Promise<PostDocument> {
     try {
       const words = input.content.split(' ').length;
       const timeToRead = Math.ceil(words / 200);
@@ -127,7 +116,7 @@ export class Posts extends MongoDataSource<PostDocument> {
       throw new Error('Failed to create post');
     }
   }
-  async updatePost({ id, input }: any) {
+  async updatePost({id, input}: any) {
     try {
       const words = input.content.split(' ').length;
       const timeToRead = Math.ceil(words / 200);
@@ -144,7 +133,7 @@ export class Posts extends MongoDataSource<PostDocument> {
     }
   }
 
-  async deletePost({ id }: { id: string }): Promise<string> {
+  async deletePost({id}: {id: string}): Promise<string> {
     try {
       await PostModel.findByIdAndDelete(id);
       return 'Post deleted successfully';
