3b3d093c4280a74c641d2fbc4f64063ba535bf78
Joonas Jouttijarvi <joonas.jouttijarvi@zoan.com>
2024-03-05 15:51:48 +0200

    feat: bookmarks page styling

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,10 +1,12 @@
 'use client';
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState, useContext, ChangeEvent} from 'react';
 import {useQuery, useApolloClient} from '@apollo/client';
 import {FETCH_USER, FETCH_POST_BY_ID} from '@/app/lib/constants';
 import {UserContext} from '@/app/contexts/usercontext';
-import {Box, Button, Flex, Spinner} from '@chakra-ui/react';
+import {Spinner} from '@chakra-ui/react';
 import {useRouter} from 'next/navigation';
+import {Box, Input, Text, VStack, Divider, IconButton} from '@chakra-ui/react';
+import {FiBookOpen} from 'react-icons/fi';
 
 interface Post {
   title: string;
@@ -24,50 +26,61 @@ interface QueryData {
 }
 
 export const UsersBookmarks: React.FC = () => {
-  const {user} = useContext(UserContext);
+  const { user } = useContext(UserContext);
   const router = useRouter();
   const client = useApolloClient();
-  const {loading, error, data} = useQuery<QueryData>(FETCH_USER, {
-    variables: {id: user?.id},
+  const { loading, error, data } = useQuery<QueryData>(FETCH_USER, {
+    variables: { id: user?.id },
     skip: !user?.id,
   });
 
   const [bookmarkedPosts, setBookmarkedPosts] = useState<Post[]>([]);
   const [fetchingPosts, setFetchingPosts] = useState(false);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [originalPosts, setOriginalPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      if (
-        data &&
-        data.user &&
-        data.user.bookmarks &&
-        data.user.bookmarks.length > 0
-      ) {
+      if (data?.user?.bookmarks?.length) {
         setFetchingPosts(true);
-        const postsPromises = data.user.bookmarks.map((bookmarkId) =>
-          client.query({
-            query: FETCH_POST_BY_ID,
-            variables: {id: bookmarkId},
-          }),
-        );
-
-        Promise.all(postsPromises)
-          .then((results) => {
-            const posts = results.map((result) => result.data.post);
-            setBookmarkedPosts(posts);
-          })
-          .catch((err) => console.error(err))
-          .finally(() => setFetchingPosts(false));
+        try {
+          const postsPromises = data.user.bookmarks.map((bookmarkId) =>
+            client.query({
+              query: FETCH_POST_BY_ID,
+              variables: { id: bookmarkId },
+            }),
+          );
+          const results = await Promise.all(postsPromises);
+          const posts = results.map((result) => result.data.post);
+          setBookmarkedPosts(posts);
+          setOriginalPosts(posts);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setFetchingPosts(false);
+        }
       }
     };
 
     fetchPosts();
   }, [data, client]);
 
-  const handleBoxClick = (slug:string) => {
-    router.push(`/blog/${slug}`);
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+    if (!newSearchTerm.trim()) {
+      setBookmarkedPosts(originalPosts);
+      return;
+    }
+    const newResults = originalPosts.filter((post) =>
+      post.title.toLowerCase().includes(newSearchTerm.toLowerCase()),
+    );
+    setBookmarkedPosts(newResults);
   };
 
+  const handleBoxClick = (slug: string) => {
+    router.push(`/blog/${slug}`);
+  };
   if (loading || fetchingPosts)
     return (
       <Box
@@ -93,30 +106,65 @@ export const UsersBookmarks: React.FC = () => {
     <Box
       display="flex"
       flexDirection="column"
-      alignItems="center"
       bg="white"
-      p={40}
-      borderRadius={8}
-      mt={19}
+      p={8}
+      borderRadius="8"
+      mt={2}
       mx="auto"
-      boxShadow="0 4px 8px 0 rgba(0,0,0,0.2)"
+      boxShadow="sm"
+      maxW="xl"
+      minW="lg"
     >
-      <Flex justifyContent="center" alignItems="center" mb={4}>
-        <h1>Bookmarks</h1>
-      </Flex>
-      {bookmarkedPosts.map((post, index) => (
-        <Button
-          key={index}
-          onClick={() => handleBoxClick(post.slug)}
-          variant="button"
+      <Box textAlign="center" mb={6}>
+        <Text fontSize="4xl" fontWeight="bold" mb={2}>
+          Your Bookmarks
+        </Text>
+        <Text fontSize="smaller">You've saved these posts for later.</Text>
+      </Box>
+      <Box mb={6}>
+        <Input
+          placeholder="Search"
           size="lg"
-          my={2}
-       >
-          {post.title}
-        </Button>
-      ))}
+          onChange={handleSearch}
+        />
+      </Box>
+      <Divider mb={6} />
+      <VStack spacing={4} align="stretch">
+        {bookmarkedPosts.map((post) => (
+          <Box
+            key={post.slug}
+            p={4}
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            bg="gray.100"
+            borderRadius="md"
+            boxShadow="md"
+            _hover={{bg: 'gray.200'}}
+          >
+            <Box flex="1" pr={4}>
+              <Text fontSize="xl" fontWeight="bold" isTruncated>
+                {post.title}
+              </Text>
+              <Text fontSize="small">Read time {post.timeToRead} min</Text>
+            </Box>
+            <Box>
+              {post.tags.map((tag, index) => (
+                <Text key={index} fontSize="x-small" color={tag.color} bg='transparent' p={1} borderRadius="md">
+                  {tag.tag}
+                </Text>
+              ))}
+            </Box>
+            <IconButton
+              aria-label={`Bookmark ${post.title}`}
+              icon={<FiBookOpen />}
+              size="lg"
+              variant="ghost"
+              onClick={() => handleBoxClick(post.slug)}
+            />
+          </Box>
+        ))}
+      </VStack>
     </Box>
   );
 };
-
-
